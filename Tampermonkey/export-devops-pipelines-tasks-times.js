c4f6b867518264d51ff8da65d65435b56c16cda7
pbiasotti <pablo.biasotti@southworks.com>
2019-02-21 19:43:01 -0300

    Add tamper monkey script

---
+++ Tampermonkey/export-devops-pipelines-tasks-times.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://dev.azure.com/azurecom/ACOM/_build/results*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var array = [];
+
+    console.log('running script')
+
+    $('div.task-log-step-content.step-log-content.flex').each(function() {
+        var valueAsString = $(this).find('.bolt-time-item > span').first().text().replace('<1s', '1s');
+        var minutes = (/([0-9]*)m/g).exec(valueAsString);
+        var seconds = (/([0-9]*)s/g).exec(valueAsString);
+        var timeInSeconds = 0;
+
+        if (minutes && minutes.length > 0)
+        {
+            timeInSeconds += parseInt(minutes[1]) * 60
+        }
+
+        if (seconds && seconds.length > 0)
+        {
+            timeInSeconds += parseInt(seconds[1])
+        }
+
+        array.push({
+            title: $(this).find('.left-section-step-title').first().text(),
+            value: timeInSeconds
+        });
+    });
+
+    const rows = [["Task", "Time"]];
+    var csvContent = '';
+
+    array.forEach(function(rowArray){
+        let row = rowArray.title + ',' + rowArray.value;
+        csvContent += row + "\r\n";
+    });
+
+
+    var a = $('<a download="data.csv" type="text/csv">Download Csv</a>');
+    a.attr("href", URL.createObjectURL(new Blob([csvContent])));
+    $(a).insertAfter('.phase-name');
+})();
+
+
